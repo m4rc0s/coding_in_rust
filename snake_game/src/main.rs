e8ea4e5c13c2253b80d2497b1e8989a9dd674146
exprcode <expressivecode@gmail.com>
2024-05-08 13:01:53 -0300

    feat: spawn components into the bevy world

--- snake_game/src/main.rs
+++ snake_game/src/main.rs
@@ -1,9 +1,25 @@
 use bevy::prelude::*;
 
+#[derive(Component)]
+struct Position {
+    x: f32,
+    y: f32,
+}
+
+#[derive(Component)]
+struct Snake;
+
+fn main() {
+    App::new()
+        .add_systems(Startup, add_snake)
+        .add_systems(Update, hello_snake)
+        .run();
+}
+
 fn hello_snake() {
     println!("Hello from bevy!");
 }
 
-fn main() {
-    App::new().add_systems(Update, hello_snake).run();
+fn add_snake(mut commands: Commands) {
+    commands.spawn((Snake, Position { y: 0.0, x: 0.0 }));
 }
