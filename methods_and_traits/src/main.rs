f34f18bd6b7cbaab0d3c9c25d0bcb53a6d0de1e8
m4rc0s <marquinhoaraujo@gmail.com>
2024-05-10 19:00:05 -0300

    note(implementing-traits): implementing trait for pet

--- methods_and_traits/src/main.rs
+++ methods_and_traits/src/main.rs
@@ -1,3 +1,32 @@
+use std::fmt::format;
+
+
+trait Pet {
+    fn talk(&self) -> String;
+    fn greet(&self) {
+        println!("Oh you're a cutie Papagaio! What's your name? {}", self.talk());
+    }
+}
+
+#[derive(Debug)]
+struct Papagaio {
+    name: String,
+    age: u8,
+}
+
+impl Pet for Papagaio {
+    fn talk(&self) -> String {
+        format!("\nPapagaio! Papagaio! Papagaio! My name is {}!", self.name)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    let loro = Papagaio {
+        name: "Loro".to_string(),
+        age: 2
+    };
+
+    loro.greet();
+
 }
